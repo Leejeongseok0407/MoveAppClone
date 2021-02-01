307ffcb93d1b24b68922955bd3a0ac0dc97b83e4
jsuk10 <jsuk10@naver.com>
2021-02-01 20:48:58 +0900

    feat : learn use map

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 function Food({ name }) {
   //return (<h3>{props.name}</h3>
-  return <h3>my name is {name}</h3>;
+  return <div>my name is {name}</div>;
 }
+
+const names = [{ name: "bob" }, { name: "jeong" }, { name: "Lee" }];
 
 function App() {
   return (
     <div>
-      <Food name="bob" />
-      <Food name="jeong" />
-      <Food name="viv" />
+      {names.map((nameString) => {
+        //<Food name={nameString.name} />;
+        return <Food name={nameString.name} />;
+      })}
     </div>
   );
 }
+
+// function App() {
+//   return (
+//     <div>
+//       <Food name="bob" />
+//       <Food name="jeong" />
+//       <Food name="viv" />
+//     </div>
+//   );
+// }
 
 export default App;
