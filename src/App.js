a5af52ded13a37831f9814f2dcb1355f825912d6
jsuk10 <jsuk10@naver.com>
2021-02-02 19:08:46 +0900

    feat : add PropTypes
    PropTypes은 인수의 타입을 체크한다. 만약 다를 경우 경고 메시지 출력

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-function Food({ name }) {
+import PropTypes from "prop-types";
+
+function Food({ name, grade }) {
   //return (<h3>{props.name}</h3>
-  return <div>my name is {name}</div>;
+  return (
+    <div>
+      <h1>my name is {name}</h1>
+      {grade}/4.5
+    </div>
+  );
 }
 
 const names = [
-  { id: 0, name: "bob" },
-  { id: 1, name: "jeong" },
-  { id: 2, name: "Lee" },
+  { id: 0, name: "bob", grade: 4.5 },
+  { id: 1, name: "jeong", grade: 3.0 },
+  { id: 2, name: "Lee", grade: 3.2 },
 ];
+
+//타입을 체크해서 경고를
+names.propTypes = {
+  name: PropTypes.string,
+  grade: PropTypes.number,
+};
 
 function renderFood(names) {
   //각각의 element는  구분할수 있는 key값(id)을 가지고 있어야함. 가지지 않을경우 에러뜸
-  return <Food key={names.id} name={names.name} />;
+  return <Food key={names.id} name={names.name} grade={names.grade} />;
 }
 
 function App() {
