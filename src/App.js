2141dcf33a179a78ddb0545942f8720fc33f0a1b
jsuk10 <jsuk10@naver.com>
2021-03-02 22:52:25 +0900

    feat : Use State and Life cycle
    state를 관리하여 data만드는 방법을 학습하였다.
    
    Life cycle(생명주기)를 통해 state에서 만든 데이터를 관리하는 방법을 학습

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
+import React from "react";
 import PropTypes from "prop-types";
 
-function Food({ name, grade }) {
-  //return (<h3>{props.name}</h3>
-  return (
-    <div>
-      <h1>my name is {name}</h1>
-      {grade}/4.5
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
 
-const names = [
-  { id: 0, name: "bob", grade: 4.5 },
-  { id: 1, name: "jeong", grade: 3.0 },
-  { id: 2, name: "Lee", grade: 3.2 },
-];
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  }
 
-//타입을 체크해서 경고를
-names.propTypes = {
-  name: PropTypes.string,
-  grade: PropTypes.number,
-};
-
-function renderFood(names) {
-  //각각의 element는  구분할수 있는 key값(id)을 가지고 있어야함. 가지지 않을경우 에러뜸
-  return <Food key={names.id} name={names.name} grade={names.grade} />;
-}
-
-function App() {
-  return <div>{names.map(renderFood)}</div>;
+  render() {
+    const { isLoading } = this.state;
+    return <div>{this.state.isLoading ? "Loading.." : "We are ready"}</div>;
+  }
 }
 
 export default App;
